81a0299c6f7e1757be327fbffdd8e7f0464a8c28
anuja-bose <anuja.joydeep.bose@gmail.com>
2022-04-30 14:27:29 +0530

    Took the version from package.json file

--- connect/connectDataFile.js
+++ connect/connectDataFile.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
+var pjson = require('../package.json');
 
 const initilizeDataFile = () => {
 
     let data = {
-        "author": "",
-        "version": "",
+        "author": "Anuja Bose",
+        "version": pjson.version,
         "time_stamp": "",
         "to_do_list": [
             { "id": "01", "task": "Shopping", "status": "Completed" },
